d4725320f476a3d2f35fc54e06e22e0d062982b4
Abhinay Jangde <abhinayjangde@gmail.com>
2024-03-07 21:47:04 +0530

    adding post method

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST','GET'])
 def index():
-    return render_template("index.html")
+    if request.method=='POST':
+        name = request.form.get("name","world")
+        return render_template("greet.html", name=name)
+    else:
+        return render_template("index.html")
 
-@app.route("/greet")
+"""
+@app.route("/greet", methods=['POST'])
 def greet():
-    name = request.args.get("name", "world")
+    # name = request.args.get("name", "world")
+    name = request.form.get("name", "world")
     return render_template("greet.html", name=name)
-    
+"""
